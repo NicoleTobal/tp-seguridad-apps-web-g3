db2e2319cd696c11e3bbf025118372daa0942d68
Nicole Tobal <nickytobal@gmail.com>
2019-05-23 21:12:08 -0300

    Se agrega endpoint para usuarios y esquema en mongoose

--- intracorp/app.js
+++ intracorp/app.js
@@ -1,8 +1,13 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const restify = require('express-restify-mongoose')
+const router = express.Router()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- intracorp/routes/users.js
+++ intracorp/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost:27017/database', { useNewUrlParser: true });
+
+// mongoose.deleteModel('User');
+
+const Schema = mongoose.Schema;
+
+const mySchema = Schema({username: String, password: String});
+const usermodel = mongoose.model('User', mySchema);
+
+usermodel.remove({}, () => {
+  usermodel.create([{username: 'laureano ', password: '1234'},
+  {username: 'nicole', password: '1234'},
+  {username: 'macha', password: '1234'},
+  {username: 'admin', password: 'admin'}], function(err, doc) {
+    if (err) return next(err);
+    console.log(doc);
+  });
+});
+
+// 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+if(req.query.password === 'distinct'){
+  usermodel.find(function(err, docs) {
+    if (err) return next(err);
+    res.send(docs);
+  });
+
+} else {
+  usermodel.find(function(err, docs) {
+    if (err) return next(err);
+    res.send(docs.map(user => user.username));
+  });
+}
+
 });
 
 module.exports = router;
