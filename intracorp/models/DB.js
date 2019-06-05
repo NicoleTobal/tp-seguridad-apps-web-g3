ee5004d01b986a8497624d4235206647d2335997
Nicole Tobal <nickytobal@gmail.com>
2019-06-04 21:12:00 -0300

    Se agrega mail al esquema de mongoose

--- intracorp/models/DB.js
+++ intracorp/models/DB.js
@@ -8,17 +8,17 @@ mongoose.connect('mongodb://localhost:27017/database', { useNewUrlParser: true }
 
 const Schema = mongoose.Schema;
 
-const mySchema = Schema({username: String, password: String});
+const mySchema = Schema({username: String, password: String, email: String});
 const usermodel = mongoose.model('User', mySchema);
 
 
 
 const initialize = function(){
   usermodel.remove({}, () => {
-    usermodel.create([{username: 'laureano', password: '1234'},
-    {username: 'nicole', password: '1234'},
-    {username: 'macha', password: '1234'},
-    {username: 'admin', password: 'admin'}], function(err, doc) {
+    usermodel.create([{username: 'laureano', password: '1234', email: "laureano.clausi@gmail.com"},
+    {username: 'nicole', password: '1234', email: "nickytobal@gmail.com"},
+    {username: 'macha', password: '1234', email: "barrimmachado@gmail.com"},
+    {username: 'admin', password: 'admin', email: "admin@admin.com"}], function(err, doc) {
       if (err) return next(err);
       console.log(doc);
     });
