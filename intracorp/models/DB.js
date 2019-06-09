0db38e7198e68ac3059ee0a4a762647ae4336a80
ramachado <ramachado@est.frba.utn.edu.ar>
2019-06-09 18:31:43 -0300

    New users

--- intracorp/models/DB.js
+++ intracorp/models/DB.js
@@ -6,10 +6,10 @@ mongoose.connect('mongodb://192.168.0.12:27017/intracorp', { useNewUrlParser: tr
 const initialize = function(){
   User.remove({}, () => {
     User.create([
-      {username: 'laureano', password: 'lau1234', email: "laureano.clausi@gmail.com"},
-      {username: 'nicole', password: 'nic1234', email: "nickytobal@gmail.com"},
-      {username: 'macha', password: 'mac1234', email: "barrimmachado@gmail.com"},
-      {username: 'admin', password: 'admin', email: "admin@admin.com"}
+      {username: 'LClausi', password: 'lau1234', email: "laureano.clausi@atlantis.com"},
+      {username: 'NTobal', password: 'nic1234', email: "nickytobal@atlantis.com"},
+      {username: 'RMachado', password: 'mac1234', email: "barrimmachado@atlantis.com"},
+      {username: 'admin', password: 'admin', email: "admin@atlantis.com"}
    ], function(err, doc) {
       if (err) return next(err);
       console.log(doc);
