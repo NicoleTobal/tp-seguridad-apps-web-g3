fde6a70593ce8c05a3545adcac84d414648645a2
Nicole Tobal <nickytobal@gmail.com>
2019-06-09 21:03:16 -0300

    Se cambian nombres y mails de usuarios

--- intracorp/models/DB.js
+++ intracorp/models/DB.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const User = require("./User.model");
 
-mongoose.connect('mongodb://192.168.0.12:27017/intracorp', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/intracorp', { useNewUrlParser: true });
 
 const initialize = function(){
   User.remove({}, () => {
     User.create([
-      {username: 'LClausi', password: 'lau1234', email: "laureano.clausi@atlantis.com"},
-      {username: 'NTobal', password: 'nic1234', email: "nickytobal@atlantis.com"},
-      {username: 'RMachado', password: 'mac1234', email: "barrimmachado@atlantis.com"},
+      {username: 'lclausi', password: 'lau1234', email: "lclausi@atlantis.com"},
+      {username: 'ntobal', password: 'nic1234', email: "ntobal@atlantis.com"},
+      {username: 'rmachado', password: 'mac1234', email: "rmachado@atlantis.com"},
       {username: 'admin', password: 'admin', email: "admin@atlantis.com"}
    ], function(err, doc) {
       if (err) return next(err);
